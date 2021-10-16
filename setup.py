bc0ed8eff8a41d4700814827704a8c61adb88b36
Andrea Censi <acensi@ethz.ch>
2021-10-16 16:05:57 +0200

    more precise dependencies

--- setup.py
+++ setup.py
@@ -19,7 +19,11 @@ def get_version(filename: str):
 
 version = get_version(filename="src/duckietown_docker_utils/__init__.py")
 
-install_requires = ["progressbar2"]
+install_requires = [
+    "progressbar2",
+    "docker",
+    "PyYAML",
+]
 
 line = "daffy"
 
