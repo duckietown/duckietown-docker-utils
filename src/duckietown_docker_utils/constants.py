7c4ddbdf64d22aacc30337fd65011fd513713cb7
Andrea Censi <acensi@ethz.ch>
2022-11-29 14:05:27 +0100

    added trailing slash for "https://pypi.org/simple" - new versions of tools like twine complain if there is a redirect

--- src/duckietown_docker_utils/constants.py
+++ src/duckietown_docker_utils/constants.py
@@ -16,7 +16,7 @@ ENV_DT_BUILD_HOST = "DT_DOCKER_BUILD_HOST"
 # If the value is None, no variable is passed if one is not present.
 IMPORTANT_ENVS = {
     ENV_REGISTRY: "docker.io",
-    "PIP_INDEX_URL": "https://pypi.org/simple",
+    "PIP_INDEX_URL": "https://pypi.org/simple/",
     "DTSERVER": "https://challenges.duckietown.org/v4",
     ENV_IGNORE_UNTAGGED: None,
     ENV_IGNORE_DIRTY: None,
